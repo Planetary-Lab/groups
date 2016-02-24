f4b8735a3199cac57b53143314a67f49a5dc539c
Christine Brown <chicagtechpro@gmail.com>
2016-02-24 17:57:57 -0600

    Update group plugin for courses and laboratory template features

--- js/groups_admin.js
+++ js/groups_admin.js
@@ -73,3 +73,78 @@ jQuery(function($){
   });
 
 });
+
+jQuery(function($){
+
+// Set all variables to be used in scope
+  var frame,
+      metaBox = $('#edit-page'), // Your meta box id here
+      addPhotoLink = metaBox.find('.upload-custom-photo'),
+      delPhotoLink = metaBox.find( '.delete-custom-photo'),
+      photoContainer = metaBox.find( '.custom-photo-container'),
+      photoIdInput = metaBox.find( '.custom-photo-id' );
+
+      // ADD IMAGE LINK
+  addPhotoLink.on( 'click', function( event ){
+    
+    event.preventDefault();
+    
+    // If the media frame already exists, reopen it.
+    if ( frame ) {
+      frame.open();
+      return;
+    }
+    
+    // Create a new media frame
+    frame = wp.media({
+      title: 'Select or Upload Media Of Your Chosen Persuasion',
+      button: {
+        text: 'Use this media'
+      },
+      multiple: false  // Set to true to allow multiple files to be selected
+    });
+
+    
+    // When an image is selected in the media frame...
+    frame.on( 'select', function() {
+      
+      // Get media attachment details from the frame state
+      var attachment = frame.state().get('selection').first().toJSON();
+
+      // Send the attachment URL to photo image input field.
+      photoContainer.append( '<img src="'+attachment.url+'" alt="" style="max-width:100%;"/>' );
+
+      // Send the attachment id to our hidden input
+      photoIdInput.val( attachment.id );
+
+      // Hide the add image link
+      addphotoLink.addClass( 'hidden' );
+
+      // Unhide the remove image link
+      delphotoLink.removeClass( 'hidden' );
+    });
+
+    // Finally, open the modal on click
+    frame.open();
+  });
+  
+  
+  // DELETE IMAGE LINK
+  delphotoLink.on( 'click', function( event ){
+
+    event.preventDefault();
+
+    // Clear out the preview image
+    photoContainer.html( '' );
+
+    // Un-hide the add image link
+    addphotoLink.removeClass( 'hidden' );
+
+    // Hide the delete image link
+    delphotoLink.addClass( 'hidden' );
+
+    // Delete the image id from the hidden input
+    photoIdInput.val( '' );
+
+  });
+});
--- lib/admin/groups-admin-pages-edit.php
+++ lib/admin/groups-admin-pages-edit.php
@@ -20,7 +20,7 @@
  */
 
 if ( !defined( 'ABSPATH' ) ) {
-	exit;
+    exit;
 }
 
 /**
@@ -29,7 +29,7 @@ if ( !defined( 'ABSPATH' ) ) {
  */
 function groups_admin_pages_edit( $page_id ) {
 
-	global $wpdb;
+    global $wpdb;
 
 
         // Check to see if the user can actually edit this page
@@ -43,7 +43,7 @@ function groups_admin_pages_edit( $page_id ) {
             }
         }
         if ( !$in_group ) {
-	    wp_die( __( 'Only group members can edit this page. You do not have sufficient permissions to edit this page.', GROUPS_PLUGIN_DOMAIN ) );
+        wp_die( __( 'Only group members can edit this page. You do not have sufficient permissions to edit this page.', GROUPS_PLUGIN_DOMAIN ) );
         }
 
         $args = array(
@@ -55,17 +55,20 @@ function groups_admin_pages_edit( $page_id ) {
         $org_logo_id = get_post_meta( $page->ID, 'organization_logo', true );
         $org_logo_src = wp_get_attachment_image_src( $org_logo_id, 'full' );
         $has_logo = is_array( $org_logo_src );
+        $lab_photo_id = get_post_meta($page->ID, 'laboratory_photo', true);
+        $lab_photo_src = wp_get_attachment_image_src( $lab_photo_id, 'full' );
+        $has_lab_photo = is_array( $lab_photo_src );
 
-	$output = '';
+    $output = '';
 
-	$current_url = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-	$current_url = remove_query_arg( 'action', $current_url );
-	$current_url = remove_query_arg( 'laboratory', $current_url );
-	$current_url = remove_query_arg( 'group_id', $current_url );
-	$current_url = remove_query_arg( 'paged', $current_url );
+    $current_url = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    $current_url = remove_query_arg( 'action', $current_url );
+    $current_url = remove_query_arg( 'laboratory', $current_url );
+    $current_url = remove_query_arg( 'group_id', $current_url );
+    $current_url = remove_query_arg( 'paged', $current_url );
 
         // Header for the form
-	$output .= '<form id="edit-page" action="' . esc_url( $current_url ) . '" method="post">';
+    $output .= '<form id="edit-page" action="' . esc_url( $current_url ) . '" method="post">';
             $output .= '<div class="group-page edit">';
                 $output .= '<h1 class="group-page-edit__title">Edit Laboratory</h1>';
 
@@ -82,11 +85,21 @@ function groups_admin_pages_edit( $page_id ) {
 
                         ob_start();
                         wp_editor( $page->post_content, 'description', array() );
-                        $output .= ob_get_clean();
+                       
+                    $output .= ob_get_clean();  
+                    $output .= '</div>'; 
+                    $output .= '<div>';
+                    // Org Map
 
+                        $output .= '<h3 class="hndle">Laboratory Directions</h3>';
+
+                        $output .= '<div class="inside">';
+                            $output .= '<input type="text" name="organization_directions" id="organization_directions" value="' . get_post_meta( $page->ID, 'organization_directions', true ) . '">';
+                        $output .= '</div>';
                     $output .= '</div>';
-                $output .= '</div>';
-
+                                     
+                    
+$output .= '</div>';
                 // Org Logo
                 $output .= '<div class="right">';
 
@@ -127,6 +140,30 @@ function groups_admin_pages_edit( $page_id ) {
                         $output .= '</div>';
                     $output .= '</div>';
 
+                    //Featured Lab Photo
+                    $output .= '<div class="postbox">';
+                        $output .= '<h3 class="hndle">Laboratory Photo</h3>';
+
+                        $output .= '<div class="inside">';
+                            $output .= '<div class="custom-photo-container">';
+                                if ( $has_lab_photo ) {
+                                    $output .= '<img src="' . $lab_photo_src[0] . '">';
+                                }
+                            $output .= '</div>';
+
+                            if ( $has_lab_photo) {
+                                $output .= '<a class="upload-custom-photo hidden" href="' . $upload_link . '">Upload Laboratory photo</a>';
+                                $output .= '<a class="delete-custom-photo" href="#">Remove Laboratory Photo</a>';
+                            } else {
+                                $output .= '<a class="upload-custom-photo" href="' . $upload_link . '">Upload Laboratory Photo</a>';
+                                $output .= '<a class="delete-custom-photo hidden" href="#">Remove Laboratory Photo</a>';
+                            }
+
+                            $output .= '<input class="custom-photo-id" name="custom-photo-id" type="hidden" value="' . esc_attr( $lab_photo_id ) . '">';
+                        $output .= '</div>';
+                    $output .= '</div>';
+
+
                     // Org Link
                     $output .= '<div class="postbox">';
                         $output .= '<h3 class="hndle">Laboratory External Link</h3>';
@@ -135,6 +172,31 @@ function groups_admin_pages_edit( $page_id ) {
                             $output .= '<input type="text" name="organization_link" id="organization_link" value="' . get_post_meta( $page->ID, 'organization_link', true ) . '">';
                         $output .= '</div>';
                     $output .= '</div>';
+
+                    // Org Address
+                    $output .= '<div class="postbox">';
+                        $output .= '<h3 class="hndle">Laboratory Address</h3>';
+
+                        $output .= '<div class="inside">';
+                            $output .= '<input type="text" name="organization_address" id="organization_address" value="' . get_post_meta( $page->ID, 'organization_address', true ) . '">';
+                        $output .= '</div>';
+                    $output .= '</div>';
+
+                    // Org contact
+                    $output .= '<div class="postbox">';
+                        $output .= '<h3 class="hndle">Laboratory Contact Info</h3>';
+
+                        $output .= '<div class="inside">';
+                            $output .= '<label for="email">Email</label><input type="text" name="organization_email" id="organization_email" value="' . get_post_meta( $page->ID, 'organization_email', true ) . '">';
+                        $output .= '</div>';
+                        $output .= '<div class="inside">';
+                            $output .= '<label for="phone">Phone</label><input type="text" name="organization_phone" id="organization_phone" value="' . get_post_meta( $page->ID, 'organization_phone', true ) . '">';
+                        $output .= '</div>';
+                         $output .= '<div class="inside">';
+                            $output .= '<label for="fax">Fax</label><input type="text" name="organization_fax" id="organization_fax" value="' . get_post_meta( $page->ID, 'organization_fax', true ) . '">';
+                        $output .= '</div>';
+                    $output .= '</div>';
+
 
                     // Groups Users
                     $output .= '<div class="postbox">';
@@ -166,27 +228,34 @@ function groups_admin_pages_edit( $page_id ) {
 
         // Form Footer
             $output .= '</div>'; // .group.edit
-	$output .= '</form>';
+    $output .= '</form>';
 
-	echo $output;
+    echo $output;
 } // function groups_admin_groups_edit
 
 /**
  * Handle edit form submission.
  */
 function groups_admin_pages_edit_submit() {
-	global $wpdb;
+    global $wpdb;
 
 
-	if ( !wp_verify_nonce( $_POST[GROUPS_ADMIN_PAGES_NONCE_1],  'groups-pages-edit' ) ) {
-		wp_die( __( 'Access denied.', GROUPS_PLUGIN_DOMAIN ) );
-	}
+    if ( !wp_verify_nonce( $_POST[GROUPS_ADMIN_PAGES_NONCE_1],  'groups-pages-edit' ) ) {
+        wp_die( __( 'Access denied.', GROUPS_PLUGIN_DOMAIN ) );
+    }
 
         if ($_POST) {
             $title = $_POST['title'];
             $description = $_POST['description'];
             $logo = $_POST['custom-img-id'];
+            $lab_photo = $_POST['custom-photo-id'];
             $link = $_POST['organization_link'];
+            $address = $_POST['organization_address'];
+            $email = $_POST['organization_email'];
+            $phone = $_POST['organization_phone'];
+            $fax = $_POST['organization_fax'];
+            $map = $_POST['organization_map'];
+            $directions = $_POST['organization_directions'];
             $page_id = $_POST['page_id'];
             $updates = array();
 
@@ -206,12 +275,35 @@ function groups_admin_pages_edit_submit() {
                 $updates['post_content'] = $description;
                 $group_id = groups_group::update( array( 'group_id' => $group->id, "description" => $description ) );
             }
+            
             if ( get_post_meta( $page->ID, 'organization_logo', true ) != $logo ) {
                 update_post_meta( $page->ID, 'organization_logo', $logo );
+            }
+            if ( get_post_meta( $page->ID, 'laboratory_photo', true ) != $lab_photo ) {
+                update_post_meta( $page->ID, 'laboratory_photo', $lab_photo );
             }
             if ( get_post_meta( $page->ID, 'organization_link', true ) != $link ) {
                 update_post_meta( $page->ID, 'organization_link', $link );
             }
+            if ( get_post_meta( $page->ID, 'organization_address', true ) != $address ) {
+                update_post_meta( $page->ID, 'organization_address', $address );
+            }
+             if ( get_post_meta( $page->ID, 'organization_email', true ) != $email ) {
+                update_post_meta( $page->ID, 'organization_email', $email );
+            }
+            if ( get_post_meta( $page->ID, 'organization_phone', true ) != $phone ) {
+                update_post_meta( $page->ID, 'organization_phone', $phone );
+            }
+            if ( get_post_meta( $page->ID, 'organization_fax', true ) != $fax ) {
+                update_post_meta( $page->ID, 'organization_fax', $fax );
+            }
+            if ( get_post_meta( $page->ID, 'organization_map', true ) != $map ) {
+                update_post_meta( $page->ID, 'organization_map', $map );
+            }
+            if ( get_post_meta( $page->ID, 'organization_directions', true ) != $directions ) {
+                update_post_meta( $page->ID, 'organization_directions', $directions );
+            }
+
 
             if ( $members ) {
                 foreach ( $groups_users as $user ) {
